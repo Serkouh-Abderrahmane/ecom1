36923f8ff6b26d644903b4661d27b2e5a891abe0
Kiril Kirkov <kirilkirkov@Kirils-MacBook-Pro-2.local>
2026-05-20 16:40:33 +0300

    fix: handle null title/description in header templates (PHP 8 compat)
    Add null coalescing ?? '' to all htmlspecialchars() calls for $title,
    $description and $keywords in all 5 template header.php files to prevent
    'Passing null to parameter of type string is deprecated' on pages where
    these variables are not set.

--- application/views/templates/onepage/_parts/header.php
+++ application/views/templates/onepage/_parts/header.php
@@ -4,14 +4,14 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="<?= htmlspecialchars($description, ENT_QUOTES, 'UTF-8') ?>">
+        <meta name="description" content="<?= htmlspecialchars($description ?? '', ENT_QUOTES, 'UTF-8') ?>">
         <meta name="keywords" content="<?= $keywords ?>" />
-        <meta property="og:title" content="<?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?>" />
-        <meta property="og:description" content="<?= htmlspecialchars($description, ENT_QUOTES, 'UTF-8') ?>" />
+        <meta property="og:title" content="<?= htmlspecialchars($title ?? '', ENT_QUOTES, 'UTF-8') ?>" />
+        <meta property="og:description" content="<?= htmlspecialchars($description ?? '', ENT_QUOTES, 'UTF-8') ?>" />
         <meta property="og:url" content="<?= LANG_URL ?>" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="<?= isset($image) && !is_null($image) ? $image : base_url('assets/img/site-overview.png') ?>" />
-        <title><?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?></title>
+        <title><?= htmlspecialchars($title ?? '', ENT_QUOTES, 'UTF-8') ?></title>
         <link rel="stylesheet" href="<?= base_url('assets/css/font-awesome.min.css') ?>" />
         <link href="<?= base_url('templatecss/bootstrap.min.css') ?>" rel="stylesheet" />
         <link href="<?= base_url('assets/css/bootstrap-datepicker.min.css') ?>" rel="stylesheet" />
